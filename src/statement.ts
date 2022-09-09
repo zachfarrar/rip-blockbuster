dfaa07087c744c9e939e8268f41f2bc78690ed08
zachfarrar <zachfarrar.93@gmail.com>
2022-09-09 13:44:58 -0400

    finalized more flexible data system

--- src/statement.ts
+++ src/statement.ts
@@ -7,10 +7,12 @@ type MoviesAndAmounts = {
   totalAmountDue: number;
 };
 
+const db = require('./data/data-store.json');
+
 export const statement = (customer: Customer, movies: MovieCollection): string => {
   let result = `Rental Record for ${customer.name}\n`;
 
-  let movieAndAmounts = calculateMoviePrice(customer, movies);
+  const movieAndAmounts = calculateMoviePrice(customer, movies);
   for (const title in movieAndAmounts.movies) {
     result += `\t${title}\t${movieAndAmounts.movies[title]}\n`
   }
@@ -24,7 +26,7 @@ export const statement = (customer: Customer, movies: MovieCollection): string =
 export const htmlStatement = (customer: Customer, movies: MovieCollection): string => {
   let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n<ul>\n`;
 
-  let movieAndAmounts = calculateMoviePrice(customer, movies);
+  const movieAndAmounts = calculateMoviePrice(customer, movies);
   for (const title in movieAndAmounts.movies) {
     result += `\t<li>${title} - ${movieAndAmounts.movies[title]}</li>\n`
   }
@@ -35,38 +37,27 @@ export const htmlStatement = (customer: Customer, movies: MovieCollection): stri
   return result;
 };
 
-
 const calculateMoviePrice = (customer: Customer, movies: MovieCollection): MoviesAndAmounts => {
-  let movieAndAmounts: MoviesAndAmounts = {
+  const movieAndAmounts: MoviesAndAmounts = {
     movies: {},
     frequentRenterPoints: 0,
     totalAmountDue: 0
   };
-  for (const r of customer.rentals) {
-    let movie = movies[r.movieID];
-    let thisAmount = 0;
 
-    switch (movie.code) {
-      case MovieCode.REGULAR:
-        thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
-        }
-        break;
-      case MovieCode.NEW:
-        thisAmount = r.days * 3;
-        break;
-      case MovieCode.CHILDRENS:
-        thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
-        }
-        break;
+  for (const rental of customer.rentals) {
+    let movie = movies[rental.movieID];
+
+    const rentalData = db[movie.code];
+    let thisAmount = rentalData.startingPrice;
+    if (rental.days > rentalData.rentalDays) {
+      thisAmount += (rental.days - rentalData.rentalDays) * rentalData.overdueMultiplier;
     }
-    
+
     movieAndAmounts.movies[movie.title] = thisAmount;
-    movieAndAmounts.frequentRenterPoints++;
-    if (movie.code === MovieCode.NEW && r.days > 2) movieAndAmounts.frequentRenterPoints++;
+    movieAndAmounts.frequentRenterPoints += rentalData.pointsPerDay;
+    if (rentalData.daysForPoints && rental.days > rentalData.daysForPoints) {
+      movieAndAmounts.frequentRenterPoints += rentalData.bonusPoints;
+    }
     movieAndAmounts.totalAmountDue += thisAmount;
   }
   return movieAndAmounts;
